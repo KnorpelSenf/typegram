b7ec7c7a4a4a5cac90e3573b2f87e7a08def7b5b
Steffen <steffen@indigo-nms.eu>
2020-08-25 18:18:44 +0200

    Unionise `CallbackQuery`

--- types.d.ts
+++ types.d.ts
@@ -944,25 +944,34 @@ export interface LoginUrl {
   request_write_access?: Boolean;
 }
 
+export namespace CallbackQuery {
+  interface AbstractCallbackQuery {
+    /** Unique identifier for this query */
+    id: String;
+    /** Sender */
+    from: User;
+    /** Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old */
+    message?: Message;
+    /** Identifier of the message sent via the bot in inline mode, that originated the query. */
+    inline_message_id?: String;
+    /** Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent. Useful for high scores in games. */
+    chat_instance: String;
+  }
+  export interface DataCallbackQuery extends AbstractCallbackQuery {
+    /** Data associated with the callback button. Be aware that a bad client can send arbitrary data in this field. */
+    data: String;
+  }
+  export interface GameShortGameCallbackQuery extends AbstractCallbackQuery {
+    /** Short name of a Game to be returned, serves as the unique identifier for the game */
+    game_short_name: String;
+  }
+}
 /** This object represents an incoming callback query from a callback button in an inline keyboard. If the button that originated the query was attached to a message sent by the bot, the field message will be present. If the button was attached to a message sent via the bot (in inline mode), the field inline_message_id will be present. Exactly one of the fields data or game_short_name will be present.
 
 NOTE: After the user presses a callback button, Telegram clients will display a progress bar until you call answerCallbackQuery. It is, therefore, necessary to react by calling answerCallbackQuery even if no notification to the user is needed (e.g., without specifying any of the optional parameters). */
-export interface CallbackQuery {
-  /** Unique identifier for this query */
-  id: String;
-  /** Sender */
-  from: User;
-  /** Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old */
-  message?: Message;
-  /** Identifier of the message sent via the bot in inline mode, that originated the query. */
-  inline_message_id?: String;
-  /** Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent. Useful for high scores in games. */
-  chat_instance: String;
-  /** Data associated with the callback button. Be aware that a bad client can send arbitrary data in this field. */
-  data?: String;
-  /** Short name of a Game to be returned, serves as the unique identifier for the game */
-  game_short_name?: String;
-}
+export type CallbackQuery =
+  | CallbackQuery.DataCallbackQuery
+  | CallbackQuery.GameShortGameCallbackQuery;
 
 /** Upon receiving a message with this object, Telegram clients will display a reply interface to the user (act as if the user has selected the bot's message and tapped 'Reply'). This can be extremely useful if you want to create user-friendly step-by-step interfaces without having to sacrifice privacy mode.
 
@@ -1414,7 +1423,6 @@ export interface InlineQueryResultMpeg4Gif {
   /** Video duration */
   mpeg4_duration?: Integer;
   /** URL of the static (JPEG or GIF) or animated (MPEG4) thumbnail for the result */
-
   thumb_url: String;
   /** MIME type of the thumbnail, must be one of “image/jpeg”, “image/gif”, or “video/mp4”. Defaults to “image/jpeg” */
   thumb_mime_type?: "image/jpeg" | "image/gif" | "video/mp4";
