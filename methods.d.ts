6503947061fdca0c1e5149506fa6874a4d5d7510
Steffen <shtrog@gmail.com>
2020-11-06 10:30:13 +0100

    Incorporate updated method docs

--- methods.d.ts
+++ methods.d.ts
@@ -105,7 +105,7 @@ export interface Telegram {
   /** A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object. */
   getMe(): UserFromGetMe;
 
-  /** Use this method to log out from the cloud Bot API server before launching the bot locally. You must log out the bot before running it locally, otherwise there is no guarantee that the bot will receive updates. After a successful call, you will not be able to log in again using the same token for 10 minutes. Returns True on success. Requires no parameters. */
+  /** Use this method to log out from the cloud Bot API server before launching the bot locally. You must log out the bot before running it locally, otherwise there is no guarantee that the bot will receive updates. After a successful call, you can immediately log in on a local server, but will not be able to log in back to the cloud Bot API server for 10 minutes. Returns True on success. Requires no parameters. */
   logOut(): True;
 
   /** Use this method to close the bot instance before moving it from one local server to another. You need to delete the webhook before calling this method to ensure that the bot isn't launched again after server restart. The method will return error 429 in the first 10 minutes after the bot is launched. Returns True on success. Requires no parameters. */
