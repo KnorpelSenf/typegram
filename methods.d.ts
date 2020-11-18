cfe5799a6a53e5a7e6501252ffce98d00125d55d
KnorpelSenf <shtrog@gmail.com>
2020-11-18 22:53:13 +0100

    Incorporate updated method docs

--- methods.d.ts
+++ methods.d.ts
@@ -609,7 +609,7 @@ export interface Telegram {
   sendChatAction(args: {
     /** Unique identifier for the target chat or username of the target channel (in the format @channelusername) */
     chat_id: Integer | String;
-    /** Type of action to broadcast. Choose one, depending on what the user is about to receive: typing for text messages, upload_photo for photos, record_video or upload_video for videos, record_audio or upload_audio for audio files, upload_document for general files, find_location for location data, record_video_note or upload_video_note for video notes. */
+    /** Type of action to broadcast. Choose one, depending on what the user is about to receive: typing for text messages, upload_photo for photos, record_video or upload_video for videos, record_voice or upload_voice for voice notes, upload_document for general files, find_location for location data, record_video_note or upload_video_note for video notes. */
     action:
       | "typing"
       | "upload_photo"
