9179d27bc0ba4f7d35c277bd9013514e96dd1239
KnorpelSenf <shtrog@gmail.com>
2020-11-18 23:03:46 +0100

    Adapt sendChatAction types to new docs

--- methods.d.ts
+++ methods.d.ts
@@ -613,10 +613,13 @@ export interface Telegram {
     action:
       | "typing"
       | "upload_photo"
+      | "record_video"
       | "upload_video"
-      | "upload_audio"
+      | "record_voice"
+      | "upload_voice"
       | "upload_document"
       | "find_location"
+      | "record_video_note"
       | "upload_video_note";
   }): True;
 
