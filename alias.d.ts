3eb1f08795946cd9ed9e66c9113612fc1aaf9bf8
KnorpelSenf <shtrog@gmail.com>
2020-11-23 00:02:11 +0100

    Complete jsdoc again

--- alias.d.ts
+++ alias.d.ts
@@ -6,30 +6,30 @@ export type String = string;
 export type Boolean = boolean;
 export type True = true;
 
-type DefaultTypeGram = Typegram<InputFile>;
+type DefaultTypegram = Typegram<InputFile>;
 
-/** Utility type providing a promisified version of Telegram */
-export type Telegram = DefaultTypeGram["Telegram"];
+/** Wrapper type to bundle all methods of the Telegram API */
+export type Telegram = DefaultTypegram["Telegram"];
 /** Utility type providing the argument type for the given method name or `{}` if the method does not take any parameters */
 export type Opts<
-  M extends keyof DefaultTypeGram["Telegram"]
-> = DefaultTypeGram["Opts"][M];
-/** Wrapper type to bundle all methods of the Telegram API */
-export type TelegramP = DefaultTypeGram["TelegramP"];
+M extends keyof DefaultTypegram["Telegram"]
+> = DefaultTypegram["Opts"][M];
+/** Utility type providing a promisified version of Telegram */
+export type TelegramP = DefaultTypegram["TelegramP"];
 /** This object represents the content of a media message to be sent. It should be one of
 - InputMediaAnimation
 - InputMediaDocument
 - InputMediaAudio
 - InputMediaPhoto
 - InputMediaVideo */
-export type InputMedia = DefaultTypeGram["InputMedia"];
+export type InputMedia = DefaultTypegram["InputMedia"];
 /** Represents a photo to be sent. */
-export type InputMediaPhoto = DefaultTypeGram["InputMediaPhoto"];
+export type InputMediaPhoto = DefaultTypegram["InputMediaPhoto"];
 /** Represents a video to be sent. */
-export type InputMediaVideo = DefaultTypeGram["InputMediaVideo"];
+export type InputMediaVideo = DefaultTypegram["InputMediaVideo"];
 /** Represents an animation file (GIF or H.264/MPEG-4 AVC video without sound) to be sent. */
-export type InputMediaAnimation = DefaultTypeGram["InputMediaAnimation"];
+export type InputMediaAnimation = DefaultTypegram["InputMediaAnimation"];
 /** Represents an audio file to be treated as music to be sent. */
-export type InputMediaAudio = DefaultTypeGram["InputMediaAudio"];
+export type InputMediaAudio = DefaultTypegram["InputMediaAudio"];
 /** Represents a general file to be sent. */
-export type InputMediaDocument = DefaultTypeGram["InputMediaDocument"];
+export type InputMediaDocument = DefaultTypegram["InputMediaDocument"];
