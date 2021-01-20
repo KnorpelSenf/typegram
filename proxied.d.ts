698587f873e736f4662ead694c370382faede496
KnorpelSenf <shtrog@gmail.com>
2021-01-20 19:31:07 +0100

    Incorporate updated method docs

--- proxied.d.ts
+++ proxied.d.ts
@@ -668,13 +668,13 @@ export interface Typegram<F> {
       file_id: String;
     }): File;
 
-    /** Use this method to kick a user from a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the group on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success. */
+    /** Use this method to kick a user from a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the chat on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success. */
     kickChatMember(args: {
       /** Unique identifier for the target group or username of the target supergroup or channel (in the format @channelusername) */
       chat_id: Integer | String;
       /** Unique identifier of the target user */
       user_id: Integer;
-      /** Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever */
+      /** Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever. Applied for supergroups and channels only. */
       until_date?: Integer;
     }): True;
 
