376d7a5a46d63f8acfb5fd72280381a46b3a4b9c
KnorpelSenf <shtrog@gmail.com>
2021-03-09 14:13:26 +0100

    Rest to 5.1

--- update.d.ts
+++ update.d.ts
@@ -34,7 +34,7 @@ export namespace Update {
     forward_date?: never;
   }
 
-  interface AbstractUpdate {
+  export interface AbstractUpdate {
     /** The update's unique identifier. Update identifiers start from a certain positive number and increase sequentially. This ID becomes especially handy if you're using Webhooks, since it allows you to ignore repeated updates or to restore the correct update sequence, should they get out of order. If there are no new updates for at least a week, then identifier of the next update will be chosen randomly instead of sequentially. */
     update_id: number;
   }
