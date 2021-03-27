108f431dd7101ebf37c5a35ecf8ba1ea31f67a8c
KnorpelSenf <shtrog@gmail.com>
2021-03-27 12:19:46 +0100

    Fix

--- close.d.ts
+++ close.d.ts
@@ -3,7 +3,7 @@ type Neighborhood<T> = T extends unknown ? string & keyof T : never;
 /** Creates the boundary of a type T for a given neighborhood N */
 type Boundary<T, N extends string> = Record<Exclude<N, keyof T>, undefined>;
 /** Creates a closed type for a given type T based on its neighborhood N */
-type CloseElement<T, N extends string> = T & Boundary<T, N>;
+type CloseElement<T, N extends string> = T & Partial<Boundary<T, N>>;
 /** Creates a closed type for each element of a given union type U based on the neighborhood N */
 type CloseAll<U, N extends string> = U extends unknown
   ? CloseElement<U, N>
