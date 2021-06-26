063bb383a8f108a5ee139e58b4e7a4622e80ccc5
Abasalt_Yar <abasaltyarmohammadzey@gmail.com>
2021-06-26 16:13:00 +0430

    Added input_field_placeholder in ForceReply & ReplyKeyboardMarkup
    Added input_field_placeholder in ForceReply & ReplyKeyboardMarkup

--- callback.d.ts
+++ callback.d.ts
@@ -9,6 +9,8 @@ export interface ReplyKeyboardMarkup {
   resize_keyboard?: boolean;
   /** Requests clients to hide the keyboard as soon as it's been used. The keyboard will still be available, but clients will automatically display the usual letter-keyboard in the chat – the user can press a special button in the input field to see the custom keyboard again. Defaults to false. */
   one_time_keyboard?: boolean;
+  /** Requests clients to add a custom place holder */
+  input_field_placeholder?: string;
   /** Use this parameter if you want to show the keyboard to specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
 
   Example: A user requests to change the bot's language, bot replies to the request with a keyboard to select the new language. Other users in the group don't see the keyboard. */
@@ -100,6 +102,8 @@ The last option is definitely more attractive. And if you use ForceReply in your
 export interface ForceReply {
   /** Shows reply interface to the user, as if they manually selected the bot's message and tapped 'Reply' */
   force_reply: true;
+  /** Requests clients to add a custom place holder */
+  input_field_placeholder?: string;
   /** Use this parameter if you want to force reply from specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message. */
   selective?: boolean;
 }
