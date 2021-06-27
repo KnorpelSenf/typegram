fb30c16a95b440617d7cb30ee8c44504623957f2
kraftwerk28 <kefirchik3@gmail.com>
2021-06-27 16:23:14 +0300

    Deprecate methods

--- proxied.d.ts
+++ proxied.d.ts
@@ -670,8 +670,22 @@ export interface Typegram<F> {
       file_id: string;
     }): File;
 
-    /** Use this method to kick a user from a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the chat on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success. */
+    /**
+     * @deprecated
+     * Use this method to kick a user from a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the chat on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success. */
     kickChatMember(args: {
+      /** Unique identifier for the target group or username of the target supergroup or channel (in the format @channelusername) */
+      chat_id: number | string;
+      /** Unique identifier of the target user */
+      user_id: number;
+      /** Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever. Applied for supergroups and channels only. */
+      until_date?: number;
+      /** Pass True to delete all messages from the chat for the user that is being removed. If False, the user will be able to see messages in the group that were sent before the user was removed. Always True for supergroups and channels. */
+      revoke_messages?: boolean;
+    }): true;
+
+    /** Use this method to kick a user from a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the chat on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success. */
+    banChatMember(args: {
       /** Unique identifier for the target group or username of the target supergroup or channel (in the format @channelusername) */
       chat_id: number | string;
       /** Unique identifier of the target user */
@@ -862,8 +876,16 @@ export interface Typegram<F> {
       chat_id: number | string;
     }): ChatMember[];
 
-    /** Use this method to get the number of members in a chat. Returns Int on success. */
+    /**
+     * @deprecated
+     * Use this method to get the number of members in a chat. Returns Int on success. */
     getChatMembersCount(args: {
+      /** Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername) */
+      chat_id: number | string;
+    }): number;
+
+    /** Use this method to get the number of members in a chat. Returns Int on success. */
+    getChatMemberCount(args: {
       /** Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername) */
       chat_id: number | string;
     }): number;
