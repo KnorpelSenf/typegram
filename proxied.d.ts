8f0b2b915860ba795ade378367946e3ec5829796
KnorpelSenf <shtrog@gmail.com>
2021-07-22 16:59:51 +0200

    Pull in changes from @grammyjs/types

--- proxied.d.ts
+++ proxied.d.ts
@@ -477,7 +477,7 @@ export interface Typegram<F> {
     /** Use this method to edit live location messages. A location can be edited until its live_period expires or editing is explicitly disabled by a call to stopMessageLiveLocation. On success, if the edited message is not an inline message, the edited Message is returned, otherwise True is returned. */
     editMessageLiveLocation(args: {
       /** Required if inline_message_id is not specified. Unique identifier for the target chat or username of the target channel (in the format @channelusername) */
-      chat_id?: number;
+      chat_id?: number | string;
       /** Required if inline_message_id is not specified. Identifier of the message to edit */
       message_id?: number;
       /** Required if chat_id and message_id are not specified. Identifier of the inline message */
