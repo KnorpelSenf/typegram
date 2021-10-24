3b2429ea425b0bbd6bec3346561651b9469cc176
KnorpelSenf <shtrog@gmail.com>
2021-10-25 00:24:23 +0200

    Incorporate updated method docs

--- proxied.d.ts
+++ proxied.d.ts
@@ -102,7 +102,7 @@ export interface InputFileProxy<F> {
     /** Use this method to get current webhook status. Requires no parameters. On success, returns a WebhookInfo object. If the bot is using getUpdates, will return an object with the url field empty. */
     getWebhookInfo(): WebhookInfo;
 
-    /** A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object. */
+    /** A simple method for testing your bot's authentication token. Requires no parameters. Returns basic information about the bot in form of a User object. */
     getMe(): UserFromGetMe;
 
     /** Use this method to log out from the cloud Bot API server before launching the bot locally. You must log out the bot before running it locally, otherwise there is no guarantee that the bot will receive updates. After a successful call, you can immediately log in on a local server, but will not be able to log in back to the cloud Bot API server for 10 minutes. Returns True on success. Requires no parameters. */
@@ -1109,7 +1109,7 @@ export interface InputFileProxy<F> {
     /** Use this method to send answers to an inline query. On success, True is returned.
     No more than 50 results per query are allowed.
 
-    Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an oauth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities. */
+    Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an OAuth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities. */
     answerInlineQuery(args: {
       /** Unique identifier for the answered query */
       inline_query_id: string;
