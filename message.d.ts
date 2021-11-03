051c3283ba686f7f4d59f199a9f95966d84ab5bb
KnorpelSenf <shtrog@gmail.com>
2021-11-03 02:54:53 +0100

    Fix typo

--- message.d.ts
+++ message.d.ts
@@ -571,7 +571,7 @@ export interface ProximityAlertTriggered {
 
 /** This object represents a service message about a change in auto-delete timer settings. */
 export interface MessageAutoDeleteTimerChanged {
-  /** New auto-delete time for messages in the chat; in secondss */
+  /** New auto-delete time for messages in the chat; in seconds */
   message_auto_delete_time: number;
 }
 
