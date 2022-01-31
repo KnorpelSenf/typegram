4f4c35379e8456aacfb455a0bab1f8ef45c5086a
KnorpelSenf <shtrog@gmail.com>
2022-02-01 00:43:26 +0100

    Pull in changes from @grammyjs/types

--- proxied.d.ts
+++ proxied.d.ts
@@ -720,7 +720,7 @@ export interface Typegram<F> {
 
     /** Use this method to unban a previously banned user in a supergroup or channel. The user will not return to the group or channel automatically, but will be able to join via link, etc. The bot must be an administrator for this to work. By default, this method guarantees that after the call the user is not a member of the chat, but will be able to join it. So if the user is a member of the chat they will also be removed from the chat. If you don't want this, use the parameter only_if_banned. Returns True on success. */
     unbanChatMember(args: {
-      /** Unique identifier for the target group or username of the target supergroup or channel (in the format @username) */
+      /** Unique identifier for the target group or username of the target supergroup or channel (in the format @channelusername) */
       chat_id: number | string;
       /** Unique identifier of the target user */
       user_id: number;
@@ -1117,7 +1117,7 @@ export interface Typegram<F> {
       message_id: number;
     }): true;
 
-    /** Use this method to send static .WEBP or animated .TGS stickers. On success, the sent Message is returned. */
+    /** Use this method to send static .WEBP, animated .TGS, or video .WEBM stickers. On success, the sent Message is returned. */
     sendSticker(args: {
       /** Unique identifier for the target chat or username of the target channel (in the format @channelusername) */
       chat_id: number | string;
@@ -1153,7 +1153,7 @@ export interface Typegram<F> {
       png_sticker: F;
     }): File;
 
-    /** Use this method to create a new sticker set owned by a user. The bot will be able to edit the sticker set thus created. You must use exactly one of the fields png_sticker or tgs_sticker. Returns True on success. */
+    /** Use this method to create a new sticker set owned by a user. The bot will be able to edit the sticker set thus created. You must use exactly one of the fields png_sticker, tgs_sticker, or webm_sticker. Returns True on success. */
     createNewStickerSet(args: {
       /** User identifier of created sticker set owner */
       user_id: number;
@@ -1163,8 +1163,10 @@ export interface Typegram<F> {
       title: string;
       /** PNG image with the sticker, must be up to 512 kilobytes in size, dimensions must not exceed 512px, and either width or height must be exactly 512px. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. */
       png_sticker?: F | string;
-      /** TGS animation with the sticker, uploaded using multipart/form-data. See https://core.telegram.org/animated_stickers#technical-requirements for technical requirements */
+      /** TGS animation with the sticker, uploaded using multipart/form-data. See https://core.telegram.org/stickers#animated-sticker-requirements for technical requirements */
       tgs_sticker?: F;
+      /** WEBM video with the sticker, uploaded using multipart/form-data. See https://core.telegram.org/stickers#video-sticker-requirements for technical requirements */
+      webm_sticker?: F;
       /** One or more emoji corresponding to the sticker */
       emojis: string;
       /** Pass True, if a set of mask stickers should be created */
@@ -1173,7 +1175,7 @@ export interface Typegram<F> {
       mask_position?: MaskPosition;
     }): true;
 
-    /** Use this method to add a new sticker to a set created by the bot. You must use exactly one of the fields png_sticker or tgs_sticker. Animated stickers can be added to animated sticker sets and only to them. Animated sticker sets can have up to 50 stickers. Static sticker sets can have up to 120 stickers. Returns True on success. */
+    /** Use this method to add a new sticker to a set created by the bot. You must use exactly one of the fields png_sticker, tgs_sticker, or webm_sticker. Animated stickers can be added to animated sticker sets and only to them. Animated sticker sets can have up to 50 stickers. Static sticker sets can have up to 120 stickers. Returns True on success. */
     addStickerToSet(args: {
       /** User identifier of sticker set owner */
       user_id: number;
@@ -1181,8 +1183,10 @@ export interface Typegram<F> {
       name: string;
       /** PNG image with the sticker, must be up to 512 kilobytes in size, dimensions must not exceed 512px, and either width or height must be exactly 512px. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. */
       png_sticker?: F | string;
-      /** TGS animation with the sticker, uploaded using multipart/form-data. See https://core.telegram.org/animated_stickers#technical-requirements for technical requirements */
+      /** TGS animation with the sticker, uploaded using multipart/form-data. See https://core.telegram.org/stickers#animated-sticker-requirements for technical requirements */
       tgs_sticker?: F;
+      /** WEBM video with the sticker, uploaded using multipart/form-data. See https://core.telegram.org/stickers#video-sticker-requirements for technical requirements */
+      webm_sticker?: F;
       /** One or more emoji corresponding to the sticker */
       emojis: string;
       /** An object for position where the mask should be placed on faces */
@@ -1203,14 +1207,14 @@ export interface Typegram<F> {
       sticker: string;
     }): true;
 
-    /** Use this method to set the thumbnail of a sticker set. Animated thumbnails can be set for animated sticker sets only. Returns True on success. */
+    /** Use this method to set the thumbnail of a sticker set. Animated thumbnails can be set for animated sticker sets only. Video thumbnails can be set only for video sticker sets only. Returns True on success. */
     setStickerSetThumb(args: {
       /** Sticker set name */
       name: string;
       /** User identifier of the sticker set owner */
       user_id: number;
-      /** A PNG image with the thumbnail, must be up to 128 kilobytes in size and have width and height exactly 100px, or a TGS animation with the thumbnail up to 32 kilobytes in size; see https://core.telegram.org/animated_stickers#technical-requirements for animated sticker technical requirements. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data.. Animated sticker set thumbnail can't be uploaded via HTTP URL. */
-      thumb: F | string;
+      /** A PNG image with the thumbnail, must be up to 128 kilobytes in size and have width and height exactly 100px, or a TGS animation with the thumbnail up to 32 kilobytes in size; see https://core.telegram.org/stickers#animated-sticker-requirements for animated sticker technical requirements, or a WEBM video with the thumbnail up to 32 kilobytes in size; see https://core.telegram.org/stickers#video-sticker-requirements for video sticker technical requirements. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. More info on Sending Files ». Animated sticker set thumbnails can't be uploaded via HTTP URL. */
+      thumb?: F | string;
     }): true;
 
     /** Use this method to send answers to an inline query. On success, True is returned.
