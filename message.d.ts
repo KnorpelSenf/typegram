79a76c92c1a254b6beff40a8ecc4dc54f8e89618
Temur Usmonaliyev <63544323+Temur5319436@users.noreply.github.com>
2022-03-01 00:57:14 +0500

    Update message.d.ts

--- message.d.ts
+++ message.d.ts
@@ -11,6 +11,8 @@ export namespace Message {
     from?: User;
     /** Sender of the message, sent on behalf of a chat. For example, the channel itself for channel posts, the supergroup itself for messages from anonymous group administrators, the linked channel for messages automatically forwarded to the discussion group. For backward compatibility, the field from contains a fake sender user in non-channel chats, if the message was sent on behalf of a chat. */
     sender_chat?: Chat;
+    /** text the message was sent */
+    text: string;
     /** Date the message was sent in Unix time */
     date: number;
     /** Conversation the message belongs to */
