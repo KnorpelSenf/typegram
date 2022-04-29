e99c81b6c8e42b2097d28e66daee5e3cd4175b82
KnorpelSenf <shtrog@gmail.com>
2022-04-29 22:02:00 +0200

    Pull in changes from @grammyjs/types

--- proxied.d.ts
+++ proxied.d.ts
@@ -1028,7 +1028,7 @@ export interface Typegram<F> {
     setChatMenuButton(args: {
       /** Unique identifier for the target private chat. If not specified, default bot's menu button will be changed */
       chat_id?: number;
-      /** An object for the new bot's menu button. Defaults to MenuButtonDefault */
+      /** An object for the bot's new menu button. Defaults to MenuButtonDefault */
       menu_button?: MenuButton;
     }): true;
 
