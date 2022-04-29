8d449418ff0dd2c1992ec6b8d55b86022e83093e
KnorpelSenf <shtrog@gmail.com>
2022-04-29 22:02:00 +0200

    Incorporate updated API docs

--- proxied.d.ts
+++ proxied.d.ts
@@ -996,7 +996,7 @@ export interface InputFileProxy<F> {
     setChatMenuButton(args: {
       /** Unique identifier for the target private chat. If not specified, default bot's menu button will be changed */
       chat_id?: number;
-      /** An object for the new bot's menu button. Defaults to MenuButtonDefault */
+      /** An object for the bot's new menu button. Defaults to MenuButtonDefault */
       menu_button?: MenuButton;
     }): true;
 
