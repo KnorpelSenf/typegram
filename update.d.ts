e03fc35cc11c75c466b9e143300c701e153b4a36
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-09-26 21:19:31 +0200

    Manually deunionize `Update` for Telegraf 5

--- update.d.ts
+++ update.d.ts
@@ -41,63 +41,245 @@ export namespace Update {
     extends AbstractUpdate {
     /** New incoming message of any kind - text, photo, sticker, etc. */
     message: New & NonChannel & M;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface EditedMessageUpdate<
     M extends CommonMessageBundle = CommonMessageBundle,
   > extends AbstractUpdate {
     /** New version of a message that is known to the bot and was edited */
     edited_message: Edited & NonChannel & M;
+    message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface ChannelPostUpdate<M extends Message = Message>
     extends AbstractUpdate {
     /** New incoming channel post of any kind - text, photo, sticker, etc. */
     channel_post: New & Channel & M;
+    message?: never;
+    edited_message?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface EditedChannelPostUpdate<
     M extends CommonMessageBundle = CommonMessageBundle,
   > extends AbstractUpdate {
     /** New version of a channel post that is known to the bot and was edited */
     edited_channel_post: Edited & Channel & M;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface InlineQueryUpdate extends AbstractUpdate {
     /** New incoming inline query */
     inline_query: InlineQuery;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface ChosenInlineResultUpdate extends AbstractUpdate {
     /** The result of an inline query that was chosen by a user and sent to their chat partner. Please see our documentation on the feedback collecting for details on how to enable these updates for your bot. */
     chosen_inline_result: ChosenInlineResult;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface CallbackQueryUpdate extends AbstractUpdate {
     /** New incoming callback query */
     callback_query: CallbackQuery;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface ShippingQueryUpdate extends AbstractUpdate {
     /** New incoming shipping query. Only for invoices with flexible price */
     shipping_query: ShippingQuery;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface PreCheckoutQueryUpdate extends AbstractUpdate {
     /** New incoming pre-checkout query. Contains full information about checkout */
     pre_checkout_query: PreCheckoutQuery;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface PollUpdate extends AbstractUpdate {
     /** New poll state. Bots receive only updates about stopped polls and polls, which are sent by the bot */
     poll: Poll;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface PollAnswerUpdate extends AbstractUpdate {
     /** A user changed their answer in a non-anonymous poll. Bots receive new votes only in polls that were sent by the bot itself. */
     poll_answer: PollAnswer;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    my_chat_member?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface MyChatMemberUpdate extends AbstractUpdate {
     /** The bot's chat member status was updated in a chat. For private chats, this update is received only when the bot is blocked or unblocked by the user. */
     my_chat_member: ChatMemberUpdated;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    chat_member?: never;
+    chat_join_request?: never;
   }
   export interface ChatMemberUpdate extends AbstractUpdate {
     /** A chat member's status was updated in a chat. The bot must be an administrator in the chat and must explicitly specify “chat_member” in the list of allowed_updates to receive these updates. */
     chat_member: ChatMemberUpdated;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_join_request?: never;
   }
   export interface ChatJoinRequestUpdate extends AbstractUpdate {
     /** A request to join the chat has been sent. The bot must have the can_invite_users administrator right in the chat to receive these updates. */
-    chat_join_request?: ChatJoinRequest;
+    chat_join_request: ChatJoinRequest;
+    message?: never;
+    edited_message?: never;
+    channel_post?: never;
+    edited_channel_post?: never;
+    inline_query?: never;
+    chosen_inline_result?: never;
+    callback_query?: never;
+    shipping_query?: never;
+    pre_checkout_query?: never;
+    poll?: never;
+    poll_answer?: never;
+    my_chat_member?: never;
+    chat_member?: never;
   }
 }
 
