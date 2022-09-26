f163bfb1c208f08d18cf3982419e028a6a4db607
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-09-26 21:00:18 +0200

    Unionize `CallbackQuery` (#36)

--- markup.d.ts
+++ markup.d.ts
@@ -84,25 +84,37 @@ export interface LoginUrl {
 /** A placeholder, currently holds no information. Use BotFather to set up your game. */
 export interface CallbackGame {}
 
+export namespace CallbackQuery {
+  interface AbstractQuery {
+    /** Unique identifier for this query */
+    id: string;
+    /** Sender */
+    from: User;
+    /** Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old */
+    message?: Message;
+    /** Identifier of the message sent via the bot in inline mode, that originated the query. */
+    inline_message_id?: string;
+    /** Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent. Useful for high scores in games. */
+    chat_instance: string;
+  }
+
+  export interface DataQuery extends AbstractQuery {
+    /** Data associated with the callback button. Be aware that the message originated the query can contain no callback buttons with this data. */
+    data: string;
+  }
+
+  export interface GameQuery extends AbstractQuery {
+    /** Short name of a Game to be returned, serves as the unique identifier for the game */
+    game_short_name: string;
+  }
+}
+
 /** This object represents an incoming callback query from a callback button in an inline keyboard. If the button that originated the query was attached to a message sent by the bot, the field message will be present. If the button was attached to a message sent via the bot (in inline mode), the field inline_message_id will be present. Exactly one of the fields data or game_short_name will be present.
 
  NOTE: After the user presses a callback button, Telegram clients will display a progress bar until you call answerCallbackQuery. It is, therefore, necessary to react by calling answerCallbackQuery even if no notification to the user is needed (e.g., without specifying any of the optional parameters). */
-export interface CallbackQuery {
-  /** Unique identifier for this query */
-  id: string;
-  /** Sender */
-  from: User;
-  /** Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old */
-  message?: Message;
-  /** Identifier of the message sent via the bot in inline mode, that originated the query. */
-  inline_message_id?: string;
-  /** Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent. Useful for high scores in games. */
-  chat_instance: string;
-  /** Data associated with the callback button. Be aware that the message originated the query can contain no callback buttons with this data. */
-  data?: string;
-  /** Short name of a Game to be returned, serves as the unique identifier for the game */
-  game_short_name?: string;
-}
+export type CallbackQuery =
+  | CallbackQuery.DataQuery
+  | CallbackQuery.GameQuery
 
 /** This object represents a custom keyboard with reply options (see Introduction to bots for details and examples). */
 export interface ReplyKeyboardMarkup {
