507fe8d5535f8381d0d54674d59c136027a8206f
KnorpelSenf <shtrog@gmail.com>
2022-11-23 22:19:17 +0100

    fix: prefer type exports

--- index.d.ts
+++ index.d.ts
@@ -7,5 +7,5 @@ export * from "./menu-button";
 export * from "./message";
 export * from "./passport";
 export * from "./payment";
-export { Typegram } from "./proxied";
+export { type Typegram } from "./proxied";
 export * from "./update";
