d21456e1ef8ab2a82f112751cbfe867860d03727
KnorpelSenf <shtrog@gmail.com>
2022-12-10 10:58:33 +0100

    Incorporate updated API docs

--- proxied.d.ts
+++ proxied.d.ts
@@ -994,7 +994,7 @@ export interface InputFileProxy<F> {
       chat_id: number | string;
       /** Topic name, 1-128 characters */
       name: string;
-      /** Color of the topic icon in RGB format. Currently, must be one of 0x6FB9F0, 0xFFD67E, 0xCB86DB, 0x8EEE98, 0xFF93B2, or 0xFB6F5F */
+      /** Color of the topic icon in RGB format. Currently, must be one of 7322096 (0x6FB9F0), 16766590 (0xFFD67E), 13338331 (0xCB86DB), 9367192 (0x8EEE98), 16749490 (0xFF93B2), or 16478047 (0xFB6F5F) */
       icon_color?:
         | 0x6FB9F0
         | 0xFFD67E
