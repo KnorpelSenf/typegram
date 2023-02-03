5cf0304802d0a04ddd05aa11030ae5e4e163584b
KnorpelSenf <shtrog@gmail.com>
2023-02-03 22:37:35 +0100

    Incorporate updated API docs

--- proxied.d.ts
+++ proxied.d.ts
@@ -975,7 +975,7 @@ export interface InputFileProxy<F> {
       chat_id: number | string;
     }): number;
 
-    /** Use this method to get information about a member of a chat. The method is guaranteed to work only if the bot is an administrator in the chat. Returns a ChatMember object on success. */
+    /** Use this method to get information about a member of a chat. The method is only guaranteed to work for other users if the bot is an administrator in the chat. Returns a ChatMember object on success. */
     getChatMember(args: {
       /** Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername) */
       chat_id: number | string;
