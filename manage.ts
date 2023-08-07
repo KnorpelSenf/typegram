1e4890a8bf7074cfecc7aeb8e1a1cc5503454b80
Slava Fomin II <slava@fomin.io>
2023-08-07 11:05:51 +0300

    fix: typing of WebhookInfo (#30)
    * Fixed typing of WebhookInfo
    
    * Removed "Optional" from JSDoc

--- manage.ts
+++ manage.ts
@@ -12,15 +12,15 @@ export interface WebhookInfo {
   /** Currently used webhook IP address */
   ip_address?: string;
   /** Unix time for the most recent error that happened when trying to deliver an update via webhook */
-  last_error_date: number;
+  last_error_date?: number;
   /** Error message in human-readable format for the most recent error that happened when trying to deliver an update via webhook */
-  last_error_message: string;
+  last_error_message?: string;
   /** Unix time of the most recent error that happened when trying to synchronize available updates with Telegram datacenters */
   last_synchronization_error_date?: number;
   /** The maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery */
-  max_connections: number;
+  max_connections?: number;
   /** A list of update types the bot is subscribed to. Defaults to all update types except chat_member */
-  allowed_updates: Array<Exclude<keyof Update, "update_id">>;
+  allowed_updates?: Array<Exclude<keyof Update, "update_id">>;
 }
 
 /** This object represents a Telegram user or bot. */
